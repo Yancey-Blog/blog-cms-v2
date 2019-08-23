6938e4ed3b11ea34bbff777784a22d57f390fe78
Yancey Leo <yanceyofficial@gmail.com>
2019-08-23 22:58:35 +0800

    :sparkles: feat: finish layouts

--- src/layouts/Layouts.tsx
+++ src/layouts/Layouts.tsx
@@ -1,13 +1,89 @@
-import React from 'react'
-import CssBaseline from '@material-ui/core/CssBaseline'
-import styles from './Layouts.module.scss'
+import React, { FC, useState } from 'react'
 
-const Layout: React.FC = () => {
+import Drawer from '@material-ui/core/Drawer'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import List from '@material-ui/core/List'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import InboxIcon from '@material-ui/icons/MoveToInbox'
+import MailIcon from '@material-ui/icons/Mail'
+
+import styles from './Layouts.module.scss'
+import classNames from 'classnames'
+
+const Layouts: FC = () => {
+  const [open, setOpen] = React.useState(true)
+
+  function handleDrawerChange() {
+    setOpen(!open)
+  }
+
   return (
-    <div className={styles.layouts}>
+    <div className={styles.root}>
       <CssBaseline />
+      <div
+        className={classNames(styles.main, {
+          [styles.mainShift]: !open,
+        })}
+      >
+        <AppBar position='relative'>
+          <Toolbar>
+            <IconButton
+              color='inherit'
+              aria-label='open drawer'
+              onClick={handleDrawerChange}
+              edge='start'
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant='h6' noWrap>
+              Mini variant drawer
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <main className={styles.content}>
+          <Typography paragraph>
+            {`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
+          dolor purus non enim praesent elementum facilisis leo vel. Risus at
+          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
+          quisque non tellus. Convallis convallis tellus id interdum velit
+          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
+          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
+          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
+          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
+          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
+          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
+          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
+          faucibus et molestie ac.`.repeat(30)}
+          </Typography>
+        </main>
+      </div>
+
+      <Drawer variant='permanent'>
+        <div
+          className={classNames(styles.drawer, { [styles.foldDrawer]: !open })}
+        >
+          <List>
+            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+        </div>
+      </Drawer>
     </div>
   )
 }
 
-export default Layout
+export default Layouts
