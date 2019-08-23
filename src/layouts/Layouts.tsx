c6c87169baacb2f8cb5292c065491b5cc251d5f5
Yancey Leo <yanceyofficial@gmail.com>
2019-08-23 14:55:00 +0800

    :sparkles: feat: support sass module
    新版本 css-loader 的 option 不再支持 localIdentName 字段，该字段包含在了 modules 里

--- src/layouts/Layouts.tsx
+++ src/layouts/Layouts.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import CssBaseline from '@material-ui/core/CssBaseline'
+import styles from './Layouts.module.scss'
 
 const Layout: React.FC = () => {
   return (
-    <div className='App'>
+    <div className={styles.layouts}>
       <CssBaseline />
     </div>
   )
