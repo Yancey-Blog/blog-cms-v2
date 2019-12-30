0a9e5117dbe33dab35bc3517ac89809949c972a0
YanceyOfficial <yanceyofficial@gmail.com>
2019-12-30 19:04:26 +0800

    feat: finish OpenSource

--- src/containers/Home/OpenSource/OpenSource.tsx
+++ src/containers/Home/OpenSource/OpenSource.tsx
@@ -1,56 +1,108 @@
 import React, { FC } from 'react'
 import { graphql, QueryRenderer } from 'react-relay'
-import environment from '../../../shared/RelayEnvironment'
-import MUIDataTable, { MUIDataTableOptions, MUIDataTableColumn } from 'mui-datatables'
+import MUIDataTable, {
+  MUIDataTableOptions,
+  MUIDataTableColumn,
+  MUIDataTableMeta,
+} from 'mui-datatables'
+import PopupState, { bindTrigger, bindPopover } from 'material-ui-popup-state'
 import { DeleteOutline, Edit, AddBox } from '@material-ui/icons'
-import { FormControl, Fab } from '@material-ui/core'
+import { FormControl, Fab, Button, Popover } from '@material-ui/core'
+import environment from '../../../shared/RelayEnvironment'
 import Loading from '../../../components/Loading/Loading'
 import TableWrapper from '../../../components/TableWrapper/TableWrapper'
 import styles from './OpenSource.module.scss'
 import { formatDate } from '../../../shared/utils'
 
-const columns: MUIDataTableColumn[] = [
-  { name: '_id', label: 'Id' },
-  { name: 'title', label: 'Title' },
-  { name: 'description', label: 'Description' },
-  { name: 'url', label: 'Url' },
-  { name: 'posterUrl', label: 'PosterUrl' },
-  {
-    name: 'createdAt',
-    label: 'CreatedAt',
-    options: {
-      customBodyRender: (value: string) => <span>{formatDate(value)}</span>,
-    },
-  },
-  {
-    name: 'updatedAt',
-    label: 'UpdatedAt',
-    options: {
-      customBodyRender: (value: string) => <span>{formatDate(value)}</span>,
-    },
-  },
-  {
-    name: 'action',
-    label: 'Action',
-    options: {
-      filter: false,
-      customBodyRender(value, tableMeta) {
-        return (
-          <>
-            <FormControl>
-              <Edit style={{ marginRight: '12px', cursor: 'pointer' }} onClick={() => {}} />
-            </FormControl>
-            <FormControl>
-              <DeleteOutline style={{ cursor: 'pointer' }} onClick={() => {}} />
-            </FormControl>
-          </>
-        )
+const OpenSource: FC = () => {
+  const columns: MUIDataTableColumn[] = [
+    { name: '_id', label: 'Id' },
+    { name: 'title', label: 'Title' },
+    { name: 'description', label: 'Description' },
+    {
+      name: 'url',
+      label: 'Url',
+      options: {
+        customBodyRender: (value: string) => (
+          <Button href={value} color="secondary" target="_blank" rel="noopener noreferrer">
+            {value}
+          </Button>
+        ),
       },
     },
-  },
-]
+    {
+      name: 'posterUrl',
+      label: 'PosterUrl',
+      options: {
+        customBodyRender: (value: string, tableMeta: MUIDataTableMeta) => (
+          <PopupState variant="popover" popupId="imagePoperOver">
+            {popupState => (
+              <div>
+                <img
+                  src={value}
+                  style={{ width: '150px' }}
+                  alt={tableMeta.rowData[1]}
+                  {...bindTrigger(popupState)}
+                />
+                <Popover
+                  {...bindPopover(popupState)}
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                  }}
+                  disableRestoreFocus
+                >
+                  <img
+                    src={value}
+                    style={{ width: '800px', display: 'block' }}
+                    alt={tableMeta.rowData[1]}
+                  />
+                </Popover>
+              </div>
+            )}
+          </PopupState>
+        ),
+      },
+    },
+    {
+      name: 'createdAt',
+      label: 'CreatedAt',
+      options: {
+        customBodyRender: (value: string) => <span>{formatDate(value)}</span>,
+      },
+    },
+    {
+      name: 'updatedAt',
+      label: 'UpdatedAt',
+      options: {
+        customBodyRender: (value: string) => <span>{formatDate(value)}</span>,
+      },
+    },
+    {
+      name: 'action',
+      label: 'Action',
+      options: {
+        filter: false,
+        customBodyRender(value, tableMeta) {
+          return (
+            <div style={{ width: '60px' }}>
+              <FormControl>
+                <Edit style={{ marginRight: '12px', cursor: 'pointer' }} onClick={() => {}} />
+              </FormControl>
+              <FormControl>
+                <DeleteOutline style={{ cursor: 'pointer' }} onClick={() => {}} />
+              </FormControl>
+            </div>
+          )
+        },
+      },
+    },
+  ]
 
-const OpenSource: FC = () => {
   const options: MUIDataTableOptions = {
     filterType: 'textField',
     responsive: 'stacked',
