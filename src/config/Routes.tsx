900814bbca658eefb5ca6da89381a170143d8ed4
Yancey Leo <yanceyofficial@gmail.com>
2020-02-02 04:21:58 +0800

    feat: comment out useless routes temporarily

--- src/config/Routes.tsx
+++ src/config/Routes.tsx
@@ -23,12 +23,12 @@ interface Route {
 }
 
 const routes: Route[] = [
-  {
-    name: 'Dashboard',
-    path: 'dashboard',
-    icon: <Dashboard />,
-    component: 'DashBoard/DashBoard',
-  },
+  // {
+  //   name: 'Dashboard',
+  //   path: 'dashboard',
+  //   icon: <Dashboard />,
+  //   component: 'DashBoard/DashBoard',
+  // },
   {
     name: 'Home',
     path: 'home',
@@ -49,66 +49,66 @@ const routes: Route[] = [
         path: 'open-source',
         component: 'Home/OpenSource/OpenSource',
       },
-      {
-        name: 'Cover',
-        path: 'cover',
-        component: 'Home/Cover/Cover',
-      },
+      // {
+      //   name: 'Cover',
+      //   path: 'cover',
+      //   component: 'Home/Cover/Cover',
+      // },
     ],
   },
-  {
-    name: 'Music',
-    path: 'music',
-    icon: <Headset />,
-    children: [
-      {
-        name: 'Live Tour',
-        path: 'live-tour',
-        component: 'Music/LiveTour/LiveTour',
-      },
-      {
-        name: 'Best Album',
-        path: 'best-album',
-        component: 'Music/BestAlbum/BestAlbum',
-      },
-      {
-        name: 'Player',
-        path: 'player',
-        component: 'Music/Player/Player',
-      },
-      {
-        name: 'Yancey Music',
-        path: 'yancey-music',
-        component: 'Music/YanceyMusic/YanceyMusic',
-      },
-    ],
-  },
-  {
-    name: 'Article',
-    path: 'article-list',
-    icon: <PostAdd />,
-    component: 'Article/ArticleList/ArticleList',
-    children: [
-      {
-        name: 'Article Detail',
-        path: 'article-detail',
-        component: 'Article/ArticleDetail/ArticleDetail',
-        hideInMenu: true,
-      },
-    ],
-  },
-  {
-    name: 'CV',
-    path: 'cv',
-    icon: <LinkedIn />,
-    component: 'CV/CV',
-  },
-  {
-    name: 'Setting',
-    path: 'setting',
-    icon: <Settings />,
-    component: 'Setting/Setting',
-  },
+  // {
+  //   name: 'Music',
+  //   path: 'music',
+  //   icon: <Headset />,
+  //   children: [
+  //     {
+  //       name: 'Live Tour',
+  //       path: 'live-tour',
+  //       component: 'Music/LiveTour/LiveTour',
+  //     },
+  //     {
+  //       name: 'Best Album',
+  //       path: 'best-album',
+  //       component: 'Music/BestAlbum/BestAlbum',
+  //     },
+  //     {
+  //       name: 'Player',
+  //       path: 'player',
+  //       component: 'Music/Player/Player',
+  //     },
+  //     {
+  //       name: 'Yancey Music',
+  //       path: 'yancey-music',
+  //       component: 'Music/YanceyMusic/YanceyMusic',
+  //     },
+  //   ],
+  // },
+  // {
+  //   name: 'Article',
+  //   path: 'article-list',
+  //   icon: <PostAdd />,
+  //   component: 'Article/ArticleList/ArticleList',
+  //   children: [
+  //     {
+  //       name: 'Article Detail',
+  //       path: 'article-detail',
+  //       component: 'Article/ArticleDetail/ArticleDetail',
+  //       hideInMenu: true,
+  //     },
+  //   ],
+  // },
+  // {
+  //   name: 'CV',
+  //   path: 'cv',
+  //   icon: <LinkedIn />,
+  //   component: 'CV/CV',
+  // },
+  // {
+  //   name: 'Setting',
+  //   path: 'setting',
+  //   icon: <Settings />,
+  //   component: 'Setting/Setting',
+  // },
   {
     name: 'Agenda',
     path: 'agenda',
