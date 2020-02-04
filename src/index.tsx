ffb9b1f355075b9f75dc69a39d4e9f9d1533df45
Yancey Leo <yanceyofficial@gmail.com>
2020-02-04 22:24:16 +0800

    feat: do not use lazy temporarily

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 import { Switch, Route, Redirect, Router } from 'react-router-dom'
 import { ApolloProvider } from '@apollo/react-hooks'
@@ -7,12 +7,10 @@ import { SnackbarProvider } from 'notistack'
 import * as serviceWorker from './serviceWorker'
 import client from './shared/apolloClient'
 import history from './shared/history'
-import Loading from './components/Loading/Loading'
 import { SnackbarUtilsConfigurator } from './components/Toast/Toast'
-
-const Login = lazy(() => import('src/pages/Login/Login'))
-const Register = lazy(() => import('src/pages/Register/Register'))
-const Layouts = lazy(() => import('src/pages/Layouts/Layouts'))
+import Login from 'src/pages/Login/Login'
+import Register from 'src/pages/Register/Register'
+import Layouts from 'src/pages/Layouts/Layouts'
 
 serviceWorker.unregister()
 
@@ -29,31 +27,29 @@ ReactDOM.render(
       <SnackbarUtilsConfigurator />
       <CssBaseline />
       <Router history={history}>
-        <Suspense fallback={<Loading />}>
-          <Switch>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route path="/register">
-              <Register />
-            </Route>
-            <Route
-              path="/"
-              render={({ location }) =>
-                window.localStorage.getItem('token') ? (
-                  <Layouts />
-                ) : (
-                  <Redirect
-                    to={{
-                      pathname: '/login',
-                      state: { from: location },
-                    }}
-                  />
-                )
-              }
-            />
-          </Switch>
-        </Suspense>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route
+            path="/"
+            render={({ location }) =>
+              window.localStorage.getItem('token') ? (
+                <Layouts />
+              ) : (
+                <Redirect
+                  to={{
+                    pathname: '/login',
+                    state: { from: location },
+                  }}
+                />
+              )
+            }
+          />
+        </Switch>
       </Router>
     </SnackbarProvider>
   </ApolloProvider>,
--- src/pages/Layouts/components/Mains/Mains.tsx
+++ src/pages/Layouts/components/Mains/Mains.tsx
@@ -1,47 +1,64 @@
-import React, { FC, lazy } from 'react'
+import React, { FC } from 'react'
 import { Route } from 'react-router-dom'
-import Routes from 'src/config/Routes'
+// import Routes from 'src/config/Routes'
 import styles from './Mains.module.scss'
 
-interface IRoute {
-  path: string
-  component: string
-}
+import Announcement from 'src/containers/Home/Announcement/Announcement'
+import OpenSource from 'src/containers/Home/OpenSource/OpenSource'
+import Motto from 'src/containers/Home/Motto/Motto'
+import Agenda from 'src/containers/Agenda/Agenda'
 
-function getRoutes() {
-  const routers: IRoute[] = []
+// interface IRoute {
+//   path: string
+//   component: string
+// }
 
-  Routes.forEach(route => {
-    if (route.routes) {
-      route.routes.forEach(routeChild => {
-        routers.push({
-          path: routeChild.path,
-          component: routeChild.component,
-        })
-      })
-    } else {
-      routers.push({
-        path: route.path,
-        component: route.component as string,
-      })
-    }
-  })
+// function getRoutes() {
+//   const routers: IRoute[] = []
 
-  return routers
-}
+//   Routes.forEach(route => {
+//     if (route.routes) {
+//       route.routes.forEach(routeChild => {
+//         routers.push({
+//           path: routeChild.path,
+//           component: routeChild.component,
+//         })
+//       })
+//     } else {
+//       routers.push({
+//         path: route.path,
+//         component: route.component as string,
+//       })
+//     }
+//   })
 
-const routeList = getRoutes()
+//   return routers
+// }
+
+// const routeList = getRoutes()
 
 const Mains: FC = () => (
   <main className={styles.main}>
-    {routeList.map(route => (
+    {/* {routeList.map(route => (
       <Route
         exact
         key={route.path}
         path={`/${route.path}`}
         component={lazy(() => import(`src/containers/${route.component}`))}
       />
-    ))}
+    ))} */}
+    <Route exact path={`/announcement`}>
+      <Announcement />
+    </Route>
+    <Route exact path={`/motto`}>
+      <Motto />
+    </Route>
+    <Route exact path={`/open-source`}>
+      <OpenSource />
+    </Route>
+    <Route exact path={`/agenda`}>
+      <Agenda />
+    </Route>
   </main>
 )
 
