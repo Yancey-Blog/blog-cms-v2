85dc666269239ee2813a086012c694dadc3cbdd0
Yancey Leo <yanceyofficial@gmail.com>
2020-03-22 21:32:51 +0800

    feat: update YanceyMusicTable

--- src/containers/Music/YanceyMusic/components/YanceyMusicTable.tsx
+++ src/containers/Music/YanceyMusic/components/YanceyMusicTable.tsx
@@ -14,6 +14,10 @@ import { formatDate, stringfySearch } from 'src/shared/utils'
 import TableWrapper from 'src/components/TableWrapper/TableWrapper'
 import Loading from 'src/components/Loading/Loading'
 import ConfirmPoper from 'src/components/ConfirmPoper/ConfirmPoper'
+import {
+  popoverAnchorOrigin,
+  popoverTransformOrigin,
+} from 'src/shared/constants'
 import { IYanceyMusic } from '../types'
 
 interface Props {
@@ -63,14 +67,8 @@ const YanceyMusicTable: FC<Props> = ({
                   />
                   <Popover
                     {...bindPopover(popupState)}
-                    anchorOrigin={{
-                      vertical: 'bottom',
-                      horizontal: 'left',
-                    }}
-                    transformOrigin={{
-                      vertical: 'top',
-                      horizontal: 'center',
-                    }}
+                    anchorOrigin={popoverAnchorOrigin}
+                    transformOrigin={popoverTransformOrigin}
                     disableRestoreFocus
                   >
                     <img
