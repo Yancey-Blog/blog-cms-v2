24912caa70af47806af036ba16210acd7758dbeb
Yancey Leo <yanceyofficial@gmail.com>
2020-04-01 03:33:36 +0800

    feat: finish SecurtyIntro

--- src/containers/Settings/Security/Security.tsx
+++ src/containers/Settings/Security/Security.tsx
@@ -1,14 +1,14 @@
 import React, { FC } from 'react'
 import ChangePassword from './components/ChangePassword/ChangePassword'
 import TwoFactors from './components/TwoFactors/TwoFactors'
-import DevicesInfo from './components/DevicesInfo/DevicesInfo'
+import SecurtyIntro from './components/SecurtyIntro/SecurtyIntro'
 
 const Security: FC = () => {
   return (
     <>
+      <SecurtyIntro />
       <ChangePassword />
       <TwoFactors isTOTP={true} />
-      <DevicesInfo />
     </>
   )
 }
---
+++ src/containers/Settings/Security/components/SecurtyIntro/SecurtyIntro.tsx
@@ -0,0 +1,31 @@
+import React, { FC } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import SettingItemWrapper from '../../../components/SettingItemWrapper/SettingItemWrapper'
+
+const useStyles = makeStyles({
+  tip: {
+    position: 'absolute',
+    top: '80px',
+    width: '460px',
+    fontSize: '14px',
+    color: '#5f6368',
+  },
+})
+
+const SecurtyIntro: FC = () => {
+  const classes = useStyles()
+
+  return (
+    <SettingItemWrapper
+      title="We keep your account protected"
+      imageUrl="https://www.gstatic.com/identity/boq/accountsettingsmobile/securitycheckup_scene_green_1264x448_6d867072cd36d3c6f4c9f0b8d82837ff.png"
+    >
+      <p className={classes.tip}>
+        The Security Checkup gives you personalized recommendations to secure
+        your account.
+      </p>
+    </SettingItemWrapper>
+  )
+}
+
+export default SecurtyIntro
