91bd5cfbe9b8eb706d879a30a561d1382decef6a
Yancey Leo <yanceyofficial@gmail.com>
2020-04-04 02:14:35 +0800

    feat: add embededPlugin

--- src/containers/Post/PostConfig.tsx
+++ src/containers/Post/PostConfig.tsx
@@ -3,7 +3,14 @@ import { Paper } from '@material-ui/core'
 import 'tui-editor/dist/tui-editor.min.css'
 import 'tui-editor/dist/tui-editor-contents.min.css'
 import 'codemirror/lib/codemirror.css'
+import 'tui-chart/dist/tui-chart.css'
+import 'tui-color-picker/dist/tui-color-picker.css'
+import uml from '@toast-ui/editor-plugin-uml'
+import tableMergedCell from '@toast-ui/editor-plugin-table-merged-cell'
+import chart from '@toast-ui/editor-plugin-chart'
+import colorSyntax from '@toast-ui/editor-plugin-color-syntax'
 import { Editor } from '@toast-ui/react-editor'
+import embededPlugin from 'src/shared/editor-plugin-embeded'
 import useStyles from './styles'
 
 const PostConfig: FC = () => {
@@ -12,13 +19,14 @@ const PostConfig: FC = () => {
   return (
     <Paper className={classes.editorWrapper}>
       <Editor
+        usageStatistics={false}
         initialValue=""
         previewStyle="vertical"
         height="100%"
         initialEditType="markdown"
+        hideModeSwitch={true}
         useCommandShortcut={true}
-        // @ts-ignore
-        exts={['scrollSync', 'colorSyntax', 'uml', 'mark', 'table']}
+        plugins={[chart, tableMergedCell, uml, colorSyntax, embededPlugin]}
       />
     </Paper>
   )
