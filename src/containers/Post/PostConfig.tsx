bc3cb72de11668d5878d4ba1c6805113be00233d
Yancey Leo <yanceyofficial@gmail.com>
2020-04-04 04:24:27 +0800

    feat: update constants

--- src/containers/Post/PostConfig.tsx
+++ src/containers/Post/PostConfig.tsx
@@ -1,5 +1,8 @@
 import React, { FC, useRef, useEffect, useState } from 'react'
-import { Paper } from '@material-ui/core'
+import { useLocation } from 'react-router-dom'
+import { Paper, TextField, FormLabel } from '@material-ui/core'
+import * as Yup from 'yup'
+import { useFormik } from 'formik'
 import 'tui-editor/dist/tui-editor.min.css'
 import 'tui-editor/dist/tui-editor-contents.min.css'
 import 'codemirror/lib/codemirror.css'
@@ -10,33 +13,80 @@ import umlPlugin from '@toast-ui/editor-plugin-uml'
 import tableMergedCellPlugin from '@toast-ui/editor-plugin-table-merged-cell'
 import chartPlugin from '@toast-ui/editor-plugin-chart'
 import colorSyntaxPlugin from '@toast-ui/editor-plugin-color-syntax'
-import embededPlugin from 'src/shared/editorEmbededPlugin'
-import enhanceEditor from 'src/shared/enhanceEditor'
 import UploaderModal from 'src/components/UploaderModal/UploaderModal'
 import { UploaderRes } from 'src/components/Uploader/types'
+import embededPlugin from 'src/shared/editorEmbededPlugin'
+import enhanceEditor from 'src/shared/enhanceEditor'
+import { MARKDOWN_EDITOR_TOOLBAR_ITEMS } from 'src/shared/constants'
+import { goBack, parseSearch } from 'src/shared/utils'
+
 import useStyles from './styles'
 
 const PostConfig: FC = () => {
-  const classes = useStyles()
-  const editorRef = useRef<Editor>(null)
+  /* query */
+  const { search } = useLocation()
+  const { id } = parseSearch(search)
 
+  /* css styles */
+  const classes = useStyles()
+
+  /* editor */
+  const editorRef = useRef<Editor>(null)
   const [open, setOpen] = useState(false)
   const [image, setImage] = useState<UploaderRes>({ name: '', url: '' })
-
-  const handleChange = (file: UploaderRes) => {
+  const handleEditorImageChange = (file: UploaderRes) => {
     setImage(file)
   }
-
   const onOk = () => {
     if (editorRef.current) {
       const instance = editorRef.current.getInstance()
       instance.insertText(`\n\n![${image.name}](${image.url})`)
     }
   }
-
   useEffect(() => {
     enhanceEditor(editorRef, setOpen)
   }, [])
+
+  /* posterUrl */
+  const handlePosterImageChange = (data: UploaderRes) => {
+    setFieldValue('posterUrl', data.url)
+  }
+
+  /* formik */
+  const initialValues = {
+    title: '',
+    showTime: new Date(),
+    posterUrl: '',
+  }
+
+  const validationSchema = Yup.object().shape({
+    posterUrl: Yup.string().url().required('PostUrl is required.'),
+    title: Yup.string().required('Title is required.'),
+    summary: Yup.string().required('Summary is required.'),
+  })
+
+  const {
+    handleSubmit,
+    setFieldValue,
+    getFieldProps,
+    setValues,
+    resetForm,
+    isSubmitting,
+    errors,
+    values,
+  } = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: async (values) => {
+      if (id) {
+        // TODO:
+      } else {
+        // TODO:
+      }
+      goBack()
+      resetForm()
+    },
+  })
 
   return (
     <Paper className={classes.editorWrapper}>
@@ -48,27 +98,7 @@ const PostConfig: FC = () => {
         previewStyle="vertical"
         height="100%"
         initialEditType="markdown"
-        toolbarItems={[
-          'heading',
-          'bold',
-          'italic',
-          'strike',
-          'divider',
-          'hr',
-          'quote',
-          'divider',
-          'ul',
-          'ol',
-          'task',
-          'indent',
-          'outdent',
-          'divider',
-          'table',
-          'link',
-          'divider',
-          'code',
-          'codeblock',
-        ]}
+        toolbarItems={MARKDOWN_EDITOR_TOOLBAR_ITEMS}
         plugins={[
           chartPlugin,
           tableMergedCellPlugin,
@@ -82,7 +112,7 @@ const PostConfig: FC = () => {
       <UploaderModal
         open={open}
         onClose={setOpen}
-        onChange={handleChange}
+        onChange={handleEditorImageChange}
         onOk={onOk}
       />
     </Paper>
--- src/pages/Layouts/constants.ts
+++
@@ -1,3 +0,0 @@
-export const drawerWidth = 260
-
-export const foldDrawerWidth = 80
