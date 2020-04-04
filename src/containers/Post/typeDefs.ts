46bc9aed9ee6f32e3fba9b25dcd57e0cefd076a8
Yancey Leo <yanceyofficial@gmail.com>
2020-04-05 05:34:49 +0800

    feat: use POST_FRAGMENT for gql

--- src/containers/Post/typeDefs.ts
+++ src/containers/Post/typeDefs.ts
@@ -1,79 +1,56 @@
 import gql from 'graphql-tag'
 
+const POST_FRAGMENT = gql`
+  fragment PostFragment on PostModel {
+    _id
+    posterUrl
+    title
+    summary
+    content
+    tags
+    lastModifiedDate
+    like
+    pv
+    isPublic
+    createdAt
+    updatedAt
+  }
+`
+
 export const CREATE_ONE_POST = gql`
   mutation CreatePost($input: CreatePostInput!) {
     createPost(input: $input) {
-      _id
-      posterUrl
-      title
-      summary
-      content
-      tags
-      lastModifiedDate
-      like
-      pv
-      isPublic
-      createdAt
-      updatedAt
+      ...PostFragment
     }
   }
+  ${POST_FRAGMENT}
 `
 
 export const UPDATE_ONE_POST = gql`
   mutation UpdatePostById($input: UpdatePostInput!) {
     updatePostById(input: $input) {
-      _id
-      posterUrl
-      title
-      summary
-      content
-      tags
-      lastModifiedDate
-      like
-      pv
-      isPublic
-      createdAt
-      updatedAt
+      ...PostFragment
     }
   }
+  ${POST_FRAGMENT}
 `
 
 export const POSTS = gql`
   query GetPosts {
     getPosts {
-      _id
-      posterUrl
-      title
-      summary
-      content
-      tags
-      lastModifiedDate
-      like
-      pv
-      isPublic
-      createdAt
-      updatedAt
+      ...PostFragment
     }
   }
+  ${POST_FRAGMENT}
 `
 
 export const DELETE_ONE_POST = gql`
   mutation DeletePostById($id: ID!) {
     deletePostById(id: $id) {
-      _id
-      posterUrl
-      title
-      summary
-      content
-      tags
-      lastModifiedDate
-      like
-      pv
-      isPublic
-      createdAt
-      updatedAt
+      ...PostFragment
     }
   }
+  ${POST_FRAGMENT}
 `
 
 export const BATCH_DELETE_POSTS = gql`
