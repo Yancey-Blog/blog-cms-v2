28536dc4efcc5736cd47d1c71a49616890555dcf
Yancey Leo <yanceyofficial@gmail.com>
2020-04-05 05:53:28 +0800

    feat: add LIVE_TOUR_FRAGMENT

--- src/containers/Music/LiveTour/typeDefs.ts
+++ src/containers/Music/LiveTour/typeDefs.ts
@@ -1,64 +1,58 @@
 import gql from 'graphql-tag'
+import { BATCH_DELETE_FRAGMENT } from 'src/shared/graphqlFragment'
+
+const LIVE_TOUR_FRAGMENT = gql`
+  fragment LiveTourFragment on LiveTourModel {
+    _id
+    title
+    posterUrl
+    showTime
+    createdAt
+    updatedAt
+  }
+`
 
 export const CREATE_ONE_LIVE_TOUR = gql`
   mutation CreateLiveTour($input: CreateLiveTourInput!) {
     createLiveTour(input: $input) {
-      _id
-      title
-      posterUrl
-      showTime
-      createdAt
-      updatedAt
+      ...LiveTourFragment
     }
   }
+  ${LIVE_TOUR_FRAGMENT}
 `
 
 export const UPDATE_ONE_LIVE_TOUR = gql`
   mutation UpdateLiveTourById($input: UpdateLiveTourInput!) {
     updateLiveTourById(input: $input) {
-      _id
-      title
-      posterUrl
-      showTime
-      createdAt
-      updatedAt
+      ...LiveTourFragment
     }
   }
+  ${LIVE_TOUR_FRAGMENT}
 `
 
 export const LIVE_TOURS = gql`
   query GetLiveTours {
     getLiveTours {
-      _id
-      title
-      posterUrl
-      showTime
-      createdAt
-      updatedAt
+      ...LiveTourFragment
     }
   }
+  ${LIVE_TOUR_FRAGMENT}
 `
 
 export const DELETE_ONE_LIVE_TOUR = gql`
   mutation DeleteLiveTourById($id: ID!) {
     deleteLiveTourById(id: $id) {
-      _id
-      title
-      posterUrl
-      showTime
-      createdAt
-      updatedAt
+      ...LiveTourFragment
     }
   }
+  ${LIVE_TOUR_FRAGMENT}
 `
 
 export const BATCH_DELETE_LIVE_TOUR = gql`
   mutation DeleteLiveTours($ids: [ID!]!) {
     deleteLiveTours(ids: $ids) {
-      n
-      ok
-      deletedCount
-      ids
+      ...BatchDeleteFragment
     }
   }
+  ${BATCH_DELETE_FRAGMENT}
 `
