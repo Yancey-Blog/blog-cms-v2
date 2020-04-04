c1ebd26770b2eea70d7d8aaebd14ef343ec45b9d
Yancey Leo <yanceyofficial@gmail.com>
2020-04-05 05:51:02 +0800

    feat: add BEST_ALBUM_FRAGMENT

--- src/containers/Music/BestAlbum/typeDefs.ts
+++ src/containers/Music/BestAlbum/typeDefs.ts
@@ -1,72 +1,60 @@
 import gql from 'graphql-tag'
+import { BATCH_DELETE_FRAGMENT } from 'src/shared/graphqlFragment'
+
+const BEST_ALBUM_FRAGMENT = gql`
+  fragment BestAlbumFragment on BestAlbumModel {
+    _id
+    title
+    artist
+    coverUrl
+    mvUrl
+    releaseDate
+    createdAt
+    updatedAt
+  }
+`
 
 export const CREATE_ONE_BEST_ALBUM = gql`
   mutation CreateBestAlbum($input: CreateBestAlbumInput!) {
     createBestAlbum(input: $input) {
-      _id
-      title
-      artist
-      coverUrl
-      mvUrl
-      releaseDate
-      createdAt
-      updatedAt
+      ...BestAlbumFragment
     }
   }
+  ${BEST_ALBUM_FRAGMENT}
 `
 
 export const UPDATE_ONE_BEST_ALBUM = gql`
   mutation UpdateBestAlbumById($input: UpdateBestAlbumInput!) {
     updateBestAlbumById(input: $input) {
-      _id
-      title
-      artist
-      coverUrl
-      mvUrl
-      releaseDate
-      createdAt
-      updatedAt
+      ...BestAlbumFragment
     }
   }
+  ${BEST_ALBUM_FRAGMENT}
 `
 
 export const BEST_ALBUMS = gql`
   query GetBestAlbums {
     getBestAlbums {
-      _id
-      title
-      artist
-      coverUrl
-      mvUrl
-      releaseDate
-      createdAt
-      updatedAt
+      ...BestAlbumFragment
     }
   }
+  ${BEST_ALBUM_FRAGMENT}
 `
 
 export const DELETE_ONE_BEST_ALBUM = gql`
   mutation DeleteBestAlbumById($id: ID!) {
     deleteBestAlbumById(id: $id) {
-      _id
-      title
-      artist
-      coverUrl
-      mvUrl
-      releaseDate
-      createdAt
-      updatedAt
+      ...BestAlbumFragment
     }
   }
+  ${BEST_ALBUM_FRAGMENT}
 `
 
 export const BATCH_DELETE_BEST_ALBUMS = gql`
   mutation DeleteBestAlbums($ids: [ID!]!) {
     deleteBestAlbums(ids: $ids) {
-      n
-      ok
-      deletedCount
-      ids
+      ...BatchDeleteFragment
     }
   }
+  ${BATCH_DELETE_FRAGMENT}
 `
