ac206159e2b30fb374c0f834c5edd1ea8de3569d
Yancey Leo <yanceyofficial@gmail.com>
2020-04-05 05:41:35 +0800

    feat: define ANNOUNCEMENT_FRAGMENT

--- src/containers/Home/Announcement/typeDefs.ts
+++ src/containers/Home/Announcement/typeDefs.ts
@@ -1,47 +1,48 @@
 import gql from 'graphql-tag'
 
+const ANNOUNCEMENT_FRAGMENT = gql`
+  fragment AnnouncementFragment on AnnouncementModel {
+    _id
+    content
+    createdAt
+    updatedAt
+  }
+`
+
 export const CREATE_ONE_ANNOUNCEMENT = gql`
   mutation CreateAnnouncement($input: CreateAnnouncementInput!) {
     createAnnouncement(input: $input) {
-      _id
-      content
-      createdAt
-      updatedAt
+      ...AnnouncementFragment
     }
   }
+  ${ANNOUNCEMENT_FRAGMENT}
 `
 
 export const UPDATE_ONE_ANNOUNCEMENT = gql`
   mutation UpdateAnnouncementById($input: UpdateAnnouncementInput!) {
     updateAnnouncementById(input: $input) {
-      _id
-      content
-      createdAt
-      updatedAt
+      ...AnnouncementFragment
     }
   }
+  ${ANNOUNCEMENT_FRAGMENT}
 `
 
 export const ANNOUNCEMENTS = gql`
   query GetAnnouncements {
     getAnnouncements {
-      _id
-      content
-      createdAt
-      updatedAt
+      ...AnnouncementFragment
     }
   }
+  ${ANNOUNCEMENT_FRAGMENT}
 `
 
 export const DELETE_ONE_ANNOUNCEMENT = gql`
   mutation DeleteAnnouncementById($id: ID!) {
     deleteAnnouncementById(id: $id) {
-      _id
-      content
-      createdAt
-      updatedAt
+      ...AnnouncementFragment
     }
   }
+  ${ANNOUNCEMENT_FRAGMENT}
 `
 
 export const BATCH_DELETE_ANNOUNCEMENT = gql`
