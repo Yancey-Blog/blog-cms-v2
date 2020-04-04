8ca29e50aff7a417152c22960fb29f73faf6c092
Yancey Leo <yanceyofficial@gmail.com>
2020-04-05 05:47:34 +0800

    feat: add OPEN_SOURCE_FRAGMENT

--- src/containers/Home/OpenSource/typeDefs.ts
+++ src/containers/Home/OpenSource/typeDefs.ts
@@ -1,68 +1,59 @@
 import gql from 'graphql-tag'
+import { BATCH_DELETE_FRAGMENT } from 'src/shared/graphqlFragment'
+
+const OPEN_SOURCE_FRAGMENT = gql`
+  fragment OpenSourceFragment on OpenSourceModel {
+    _id
+    title
+    description
+    url
+    posterUrl
+    createdAt
+    updatedAt
+  }
+`
 
 export const CREATE_ONE_OPEN_SOURCE = gql`
   mutation CreateOpenSource($input: CreateOpenSourceInput!) {
     createOpenSource(input: $input) {
-      _id
-      title
-      description
-      url
-      posterUrl
-      createdAt
-      updatedAt
+      ...OpenSourceFragment
     }
   }
+  ${OPEN_SOURCE_FRAGMENT}
 `
 
 export const UPDATE_ONE_OPEN_SOURCE = gql`
   mutation UpdateOpenSourceById($input: UpdateOpenSourceInput!) {
     updateOpenSourceById(input: $input) {
-      _id
-      title
-      description
-      url
-      posterUrl
-      createdAt
-      updatedAt
+      ...OpenSourceFragment
     }
   }
+  ${OPEN_SOURCE_FRAGMENT}
 `
 
 export const OPEN_SOURCES = gql`
   query GetOpenSources {
     getOpenSources {
-      _id
-      title
-      description
-      url
-      posterUrl
-      createdAt
-      updatedAt
+      ...OpenSourceFragment
     }
   }
+  ${OPEN_SOURCE_FRAGMENT}
 `
 
 export const DELETE_ONE_OPEN_SOURCE = gql`
   mutation DeleteOpenSourceById($id: ID!) {
     deleteOpenSourceById(id: $id) {
-      _id
-      title
-      description
-      url
-      posterUrl
-      createdAt
-      updatedAt
+      ...OpenSourceFragment
     }
   }
+  ${OPEN_SOURCE_FRAGMENT}
 `
 
 export const BATCH_DELETE_OPEN_SOURCE = gql`
   mutation DeleteOpenSources($ids: [ID!]!) {
     deleteOpenSources(ids: $ids) {
-      n
-      ok
-      deletedCount
-      ids
+      ...BatchDeleteFragment
     }
   }
+  ${BATCH_DELETE_FRAGMENT}
 `
