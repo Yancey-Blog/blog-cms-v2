ea69484f2f2f1a8ef6f6c971390e9429eb6b56c8
Yancey Leo <yanceyofficial@gmail.com>
2020-04-11 23:26:37 +0800

    feat: finsih RelaeasePick component

--- src/containers/Settings/GlobalConfig/GlobalConfig.tsx
+++ src/containers/Settings/GlobalConfig/GlobalConfig.tsx
@@ -1,37 +1,84 @@
 import React, { FC } from 'react'
-import { Button, TextField } from '@material-ui/core'
+import { useLazyQuery, useQuery, useMutation } from '@apollo/react-hooks'
+import { useSnackbar } from 'notistack'
+import Loading from 'src/components/Loading/Loading'
+import { POSTS } from 'src/containers/Post/typeDefs'
+import { Query as PostsQuery } from 'src/containers/Post/types'
 import SettingsHeader from '../components/SettingsHeader/SettingsHeader'
-import SettingItemWrapper from '../components/SettingItemWrapper/SettingItemWrapper'
+import { GLOBAL_SETTING, UPDATE_GLOBAL_SETTING_BY_ID } from './typeDefs'
+import { Query } from './types'
+import ReleasePicker from './components/ReleasePicker'
+import CVPicker from './components/CVPicker'
 
 const GlobalConfig: FC = () => {
+  const { enqueueSnackbar } = useSnackbar()
+
+  const [
+    fetchPostsByPage,
+    { loading: isFetchingPosts, data: postsData },
+  ] = useLazyQuery<PostsQuery>(POSTS, {
+    notifyOnNetworkStatusChange: true,
+  })
+
+  const fetchPosts = (title: string) => {
+    fetchPostsByPage({
+      variables: {
+        input: {
+          page: 1,
+          pageSize: 10,
+          title,
+        },
+      },
+    })
+  }
+
+  const { loading: isFetching, data } = useQuery<Query>(GLOBAL_SETTING, {
+    notifyOnNetworkStatusChange: true,
+    fetchPolicy: 'cache-and-network',
+  })
+
+  const [updateGlobalSettingById, { loading: isSubmitting }] = useMutation(
+    UPDATE_GLOBAL_SETTING_BY_ID,
+    {
+      errorPolicy: 'all',
+      onCompleted() {
+        enqueueSnackbar('Update success!', { variant: 'success' })
+      },
+      onError() {},
+    },
+  )
+
   return (
     <>
-      <SettingsHeader
-        title="Global Config"
-        subTitle="Basic info, like your name and photo, that you use on Yancey Blog CMS services"
-      />
+      {isFetching ? (
+        <Loading />
+      ) : (
+        <>
+          <SettingsHeader
+            title="Global Config"
+            subTitle="Basic info, like your name and photo, that you use on Yancey Blog CMS services"
+          />
 
-      <SettingItemWrapper title="Version Release Post">
-        <Button
-          color="primary"
-          variant="contained"
-          type="submit"
-          // disabled={isSubmitting}
-        >
-          Submit
-        </Button>
-      </SettingItemWrapper>
-
-      <SettingItemWrapper title="Self Introduction Post">
-        <Button
-          color="primary"
-          variant="contained"
-          type="submit"
-          // disabled={isSubmitting}
-        >
-          Submit
-        </Button>
-      </SettingItemWrapper>
+          <ReleasePicker
+            id={data ? data.getGlobalSetting._id : ''}
+            updateGlobalSettingById={updateGlobalSettingById}
+            releasePostId={data ? data.getGlobalSetting.releasePostId : ''}
+            isFetching={isFetchingPosts}
+            isSubmitting={isSubmitting}
+            fetchPosts={fetchPosts}
+            posts={postsData ? postsData.getPosts.items : []}
+          />
+          <CVPicker
+            id={data ? data.getGlobalSetting._id : ''}
+            updateGlobalSettingById={updateGlobalSettingById}
+            cvPostId={data ? data.getGlobalSetting.cvPostId : ''}
+            isFetching={isFetchingPosts}
+            isSubmitting={isSubmitting}
+            fetchPosts={fetchPosts}
+            posts={postsData ? postsData.getPosts.items : []}
+          />
+        </>
+      )}
     </>
   )
 }
---
+++ src/containers/Settings/GlobalConfig/typeDefs.ts
@@ -0,0 +1,28 @@
+import gql from 'graphql-tag'
+
+const GLOBAL_SETTING_FRAGMENT = gql`
+  fragment GlobalSettingFragment on GlobalSettingModel {
+    _id
+    releasePostId
+    cvPostId
+    isGrayTheme
+  }
+`
+
+export const GLOBAL_SETTING = gql`
+  query GetGlobalSetting {
+    getGlobalSetting {
+      ...GlobalSettingFragment
+    }
+  }
+  ${GLOBAL_SETTING_FRAGMENT}
+`
+
+export const UPDATE_GLOBAL_SETTING_BY_ID = gql`
+  mutation UpdateGlobalSettingById($input: UpdateGlobalSettingInput!) {
+    updateGlobalSettingById(input: $input) {
+      ...GlobalSettingFragment
+    }
+  }
+  ${GLOBAL_SETTING_FRAGMENT}
+`
