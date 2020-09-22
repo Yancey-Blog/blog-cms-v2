c09b39f1ab76b8150daba8a01aaa806ba35cd720
Yancey Leo <yanceyofficial@gmail.com>
2020-09-22 15:45:08 +0800

    feat(extends): support external link to Drawer component

---
+++ src/containers/Events/components/CustomTodayButton.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from 'react'
+import { Button } from '@material-ui/core'
+import { TodayButton } from '@devexpress/dx-react-scheduler-material-ui'
+import useStyles from '../styles'
+
+const CustomTodayButton: FC<TodayButton.ButtonProps> = ({ setCurrentDate }) => {
+  const classes = useStyles()
+
+  return (
+    <Button
+      size="small"
+      variant="contained"
+      color="primary"
+      onClick={() => setCurrentDate(new Date())}
+      className={classes.customBtn}
+    >
+      Today
+    </Button>
+  )
+}
+
+export default CustomTodayButton
