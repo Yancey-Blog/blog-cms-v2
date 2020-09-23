5a3e2368df3d5b5c7f0487f783f7358ca52172c4
Yancey Leo <yanceyofficial@gmail.com>
2020-09-23 15:08:46 +0800

    feat: send data after publishing an article

--- src/containers/Post/algolia/algoliaSearch.ts
+++ src/containers/Post/algolia/algoliaSearch.ts
@@ -25,7 +25,6 @@ export const sendPostToAlgolia = async (
     await index.saveObject(
       {
         objectID,
-        url: `https://yanceyleo.com/post/${objectID}`,
         name,
         description,
         content,
