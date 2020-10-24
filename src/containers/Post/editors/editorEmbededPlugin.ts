5f5480d276374495a335f4691efaa624b245ad11
Yancey Leo <yanceyofficial@gmail.com>
2020-10-24 16:06:13 +0800

    feat: update editorEmbededPlugin

--- src/containers/Post/editors/editorEmbededPlugin.ts
+++ src/containers/Post/editors/editorEmbededPlugin.ts
@@ -1,17 +1,19 @@
 import ToastUIEditor from '@toast-ui/editor'
 import { randomSeries } from 'yancey-js-util'
 
-const renderEmbeded = (wrapperId: string, iframeEl: string) => {
+const getEmbededEL = (wrapperId: string, iframeEl: string) => {
   const el = document.querySelector(`#${wrapperId}`)
   if (el) {
     el.innerHTML = iframeEl
   }
 }
 
-export default () => {
+const renderEmbeded = () => {
   ToastUIEditor.codeBlockManager.setReplacer('embeded', (iframeEl: string) => {
     const wrapperId = `embeded_${randomSeries(6)}`
-    setTimeout(renderEmbeded.bind(null, wrapperId, iframeEl), 0)
+    setTimeout(getEmbededEL.bind(null, wrapperId, iframeEl), 0)
     return `<div id="${wrapperId}"></div>`
   })
 }
+
+export default renderEmbeded
