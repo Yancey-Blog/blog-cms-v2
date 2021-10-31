c65c2bfe843816fd14889043a3b687a7da39aae1
Yancey Leo <yanceyofficial@gmail.com>
2021-10-31 18:47:01 +0800

    feat: deploy for remote

--- src/containers/DashBoard/components/StatusCard.tsx
+++ src/containers/DashBoard/components/StatusCard.tsx
@@ -33,6 +33,9 @@ const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     card: {
       padding: 16,
+      boxShadow:
+        'rgb(145 158 171 / 24%) 0px 0px 2px 0px, rgb(145 158 171 / 24%) 0px 16px 32px -4px',
+      borderRadius: 16,
     },
 
     cardTitle: {
--- src/containers/DashBoard/components/TagClouds.tsx
+++ src/containers/DashBoard/components/TagClouds.tsx
@@ -12,6 +12,9 @@ const useStyles = makeStyles((theme: Theme) =>
     paper: {
       padding: 16,
       overflowY: 'scroll',
+      boxShadow:
+        'rgb(145 158 171 / 24%) 0px 0px 2px 0px, rgb(145 158 171 / 24%) 0px 16px 32px -4px',
+      borderRadius: 16,
     },
 
     chip: {
